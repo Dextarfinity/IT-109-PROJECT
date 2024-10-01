aa9c97734a583e39683fc01d1f3e55ae2d3991a2
dextarfinity <xdfeverharsh@gmail.com>
2024-10-01 17:50:26 +0800

    new commit

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -4,9 +4,14 @@
 <meta charset="UTF-8"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>{{ config('app.name') }}</title> 
+<style>
+html{
+    overflow-x: none;
+}
+</style>
 @vite(['resources/css/app.css', 'resources/js/app.js']) 
 </head> 
-<body class="antialiased"> 
+<body class="antialiased leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;"> 
 <div id="app"></div> 
 </body> 
 </html>
